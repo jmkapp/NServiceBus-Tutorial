5e3a5e5d71f7935e720bdeb04784cd51aa565bcc
Jeremy Kapp <github@jezzer.me>
2018-01-14 10:48:29 +0000

    Lesson 2 - working

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using NServiceBus;
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -17,11 +19,47 @@ namespace ClientUI
             var endpointInstance = await Endpoint.Start(endpointConfiguration)
                 .ConfigureAwait(false);
 
-            Console.WriteLine("Press Enter to exit...");
-            Console.ReadLine();
+            await RunLoop(endpointInstance)
+                .ConfigureAwait(false);
 
             await endpointInstance.Stop()
                 .ConfigureAwait(false);
+        }
+
+        static ILog log = LogManager.GetLogger<Program>();
+
+        static async Task RunLoop(IEndpointInstance endpointInstance)
+        {
+            while (true)
+            {
+                log.Info("Press 'P' to place an order, or 'Q' to quit.");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        // Instantiate the command
+                        var command = new PlaceOrder
+                        {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+
+                        // Send the command to the local endpoint
+                        log.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+                        await endpointInstance.SendLocal(command)
+                            .ConfigureAwait(false);
+
+                        break;
+
+                    case ConsoleKey.Q:
+                        return;
+
+                    default:
+                        log.Info("Unknown input. Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
